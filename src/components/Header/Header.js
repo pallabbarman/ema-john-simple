da0c830348e1839ea47f36604ebcc202e836f6e0
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-03-16 22:56:43 +0600

    shipment added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { userContext } from '../../App';
 import logo from '../../images/logo.png';
 import './Header.css';
 
-const Header = () => (
+const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(userContext);
+    return(
     <div className="header">
         <img src={logo} alt="" />
         <nav>
             <Link to="/shop">Shop</Link>
             <Link to="/review">Review</Link>
             <Link to="/inventory">Manage Inventory</Link>
+            <button onClick={()=>setLoggedInUser({})}>Sign Out</button>
         </nav>
     </div>
 );
+    }
 
 export default Header;
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { userContext } from "../../App";
+import "./Shipment.css";
 
 const Shipment = () => {
     const { register, handleSubmit, watch, errors } = useForm();
+    const [loggedInUser, setLoggedInUser] = useContext(userContext);
     const onSubmit = (data) => console.log(data);
 
-    console.log(watch('example'));
+    console.log(watch("example"));
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input name="example" defaultValue="test" ref={register} />
-            <input name="exampleRequired" ref={register({ required: true })} />
-            {errors.exampleRequired && <span>This field is required</span>}
+        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+            <input
+                name="name"
+                defaultValue={loggedInUser.name}
+                ref={register({ required: true })}
+                placeholder="Your Name"
+            />
+            {errors.name && <span className="error">Name is required</span>}
+            <input
+                name="email"
+                defaultValue={loggedInUser.email}
+                ref={register({ required: true })}
+                placeholder="Your Email"
+            />
+            {errors.email && <span className="error">Email is required</span>}
+            <input
+                name="address"
+                ref={register({ required: true })}
+                placeholder="Your Address"
+            />
+            {errors.address && (
+                <span className="error">Address is required</span>
+            )}
+            <input
+                name="phone"
+                ref={register({ required: true })}
+                placeholder="Your Phone Number"
+            />
+            {errors.phone && (
+                <span className="error">Phone Number is required</span>
+            )}
             <input type="submit" />
         </form>
     );
