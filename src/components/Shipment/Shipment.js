b893fd33930274b4babdcba3a016777102845291
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-03-28 12:25:18 +0600

    shipment added

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,12 +1,32 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { userContext } from "../../App";
+import { getDatabaseCart, processOrder } from "../../utilities/databaseManager";
 import "./Shipment.css";
 
 const Shipment = () => {
     const { register, handleSubmit, watch, errors } = useForm();
     const [loggedInUser, setLoggedInUser] = useContext(userContext);
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data) => {
+        const savedCart = getDatabaseCart();
+        const orderDetails = {
+            ...loggedInUser,
+            products: savedCart,
+            shipment: data,
+            orderTime: new Date(),
+        };
+
+        fetch("http://localhost:5000/addOrder", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(orderDetails),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                processOrder();
+                alert("Your order placed successfully");
+            });
+    };
 
     console.log(watch("example"));
     return (
